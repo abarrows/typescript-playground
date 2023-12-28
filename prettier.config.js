aff27cf2af59cd60a4a2829f57982de9ce63b46e
Andrew Barrows <abarrows@amuniversal.com>
2023-12-28 16:25:10 -0600

    Finally got eslint tuned with most of my normal rulesets.

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
-  trailingComma: "es5",
+  singleQuote: true,
   tabWidth: 2,
-  printWidth: 120,
-  semi: false,
+  trailingComma: "all",
+  jsxSingleQuote: true,
 }
